ca9fe94bdcc6b1a080c9bd73729ec21251df9ead
Michael L Petersen <ekmlpe@danskespil.dk>
2025-08-26 15:48:24 +0200

    :bus:

--- src/background.js
+++ src/background.js
@@ -21,19 +21,101 @@ chrome.runtime.onInstalled.addListener(reason => {
 
 });
 
+
+// Listen for messages from content script
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.action === "executePost") {
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then(response => response.json())
+      .then(apiData => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: (data) => {
+              // Use the API data in the page context
+              console.log('POST: ', data);
+              // You can call functions or update the DOM here
+            },
+            args: [apiData]
+          });
+        });
+      });
+  }
+
+  if (message.action === "executeCode") {
+    // fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js')
+    //   .then(response => response.text())
+    //   .then(jsCode => {
+    //     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //       chrome.scripting.executeScript({
+    //         target: { tabId: tabs[0].id },
+    //         func: (code) => { (new Function(code))(); },
+    //         args: [jsCode]
+    //       });
+    //     });
+    //   });
+
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+    // fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js')
+      .then(response => response.json())
+      .then(apiData => {
+        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+          chrome.scripting.executeScript({
+            target: { tabId: tabs[0].id },
+            func: (data) => {
+              // Use the API data in the page context
+              console.log('GET: ', data);
+              // You can call functions or update the DOM here
+            },
+            args: [apiData]
+          });
+        });
+      });
+  }
+});
+
+
 // background.js (Manifest V3)
 // chrome.action.onClicked.addListener(async (tab) => {
 // Fetch JS code from your API
-// const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js?token=GHSAT0AAAAAADCAFXIYQ4PZB2KLNUXCP7JI2FNWLGA');
-// const jsCode = await response.text();
+// async function init() {
+//   const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js');
+//   const jsCode = await response.text();
 //
 // // Inject and execute the code in the current tab
-// chrome.scripting.executeScript({
-//   target: { tabId: tab.id },
-//   func: (code) => {
-//     // Use Function constructor to execute fetched code
-//     (new Function(code))();
-//   },
-//   args: [jsCode]
+//   chrome.scripting.executeScript({
+//     target: { tabId: tab.id },
+//     func: (code) => {
+//       // Use Function constructor to execute fetched code
+//       (new Function(code))();
+//     },
+//     args: [jsCode]
+//   });
+// }
+//
+// await init();
+
 // });
-//});
+
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.action === "executeCode") {
+//     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//       chrome.scripting.executeScript({
+//         target: { tabId: tabs[0].id },
+//         func: (code) => { (new Function(code))(); },
+//         args: [message.code]
+//       });
+//     });
+//   }
+// });
--- src/content.js
+++ src/content.js
@@ -6,6 +6,23 @@
     }
   };
 
+  function executeExternalScript() {
+    chrome.runtime.sendMessage({ action: "executeCode" });
+  }
+
+  function executeExternalPost() {
+    chrome.runtime.sendMessage({ action: "executePost" });
+  }
+
+  window.onload = () => {
+    if (location.host === 'sixth.dk') {
+      executeExternalScript();
+      executeExternalPost();
+    }
+  };
+  
+  
+
   async function init() {
     chrome.storage.sync.get("selectedFooBar", ({ selectedFooBar }) => {
 
@@ -13,18 +30,24 @@
 
     });
 
-    const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js?token=GHSAT0AAAAAADCAFXIYQ4PZB2KLNUXCP7JI2FNWLGA');
-    const jsCode = await response.text();
-
-    // Inject and execute the code in the current tab
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      func: (code) => {
-        // Use Function constructor to execute fetched code
-        (new Function(code))();
-      },
-      args: [jsCode]
-    });
+    //   const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js');
+    //   const jsCode = await response.text();
+    //
+    //   // Send code to background script for execution
+    //   chrome.runtime.sendMessage({ action: "executeCode", code: jsCode });
+    //
+    //   // Inject and execute the code in the current tab
+    //   // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    //   //   chrome.scripting.executeScript({
+    //   //     target: { tabId: tabs[0].id },
+    //   //     function: (code) => {
+    //   //       // Use Function constructor to execute fetched code
+    //   //       (new Function(code))();
+    //   //     },
+    //   //     args: [jsCode]
+    //   //   });
+    //   // });
   }
 
 })();
+
