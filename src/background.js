14db7905be1f7747a52778287cd124a981c25e62
Michael L Petersen <ekmlpe@danskespil.dk>
2025-08-26 15:13:44 +0200

    :bus:

--- src/background.js
+++ src/background.js
@@ -24,16 +24,16 @@ chrome.runtime.onInstalled.addListener(reason => {
 // background.js (Manifest V3)
 // chrome.action.onClicked.addListener(async (tab) => {
 // Fetch JS code from your API
-const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js?token=GHSAT0AAAAAADCAFXIYQ4PZB2KLNUXCP7JI2FNWLGA');
-const jsCode = await response.text();
-
-// Inject and execute the code in the current tab
-chrome.scripting.executeScript({
-  target: { tabId: tab.id },
-  func: (code) => {
-    // Use Function constructor to execute fetched code
-    (new Function(code))();
-  },
-  args: [jsCode]
-});
+// const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js?token=GHSAT0AAAAAADCAFXIYQ4PZB2KLNUXCP7JI2FNWLGA');
+// const jsCode = await response.text();
+//
+// // Inject and execute the code in the current tab
+// chrome.scripting.executeScript({
+//   target: { tabId: tab.id },
+//   func: (code) => {
+//     // Use Function constructor to execute fetched code
+//     (new Function(code))();
+//   },
+//   args: [jsCode]
+// });
 //});
--- src/content.js
+++ src/content.js
@@ -6,14 +6,25 @@
     }
   };
 
-  function init() {
+  async function init() {
     chrome.storage.sync.get("selectedFooBar", ({ selectedFooBar }) => {
 
-      alert(`Chrome Plugin works, selected fooBar is ${selectedFooBar.foo}.`);
+      // alert(`Chrome Plugin works, selected fooBar is ${selectedFooBar.foo}.`);
 
     });
-    
-    
+
+    const response = await fetch('https://raw.githubusercontent.com/tennisfar/ChromePluginJavascriptExecution/refs/heads/master/src/script.js?token=GHSAT0AAAAAADCAFXIYQ4PZB2KLNUXCP7JI2FNWLGA');
+    const jsCode = await response.text();
+
+    // Inject and execute the code in the current tab
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: (code) => {
+        // Use Function constructor to execute fetched code
+        (new Function(code))();
+      },
+      args: [jsCode]
+    });
   }
 
 })();
